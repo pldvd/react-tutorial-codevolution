4ece2a63765b3535d54bb0e695a8ff1c27f9e08d
David Pal <paldavid2030@gmail.com>
2019-07-08 15:49:30 +0300

    make app component a class component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react'
 import {Hello} from './components/Hello';
 import Welcome from './components/Welcome';
 import Message from './components/Message'
@@ -7,15 +7,19 @@ import './App.css';
 
 
 //we can add properties(props) to React elements defined in the JSX as element attributes, these props will we passed on to the child element for use as either a function parameter in a functional component or this.props.propName in a class component
-function App() {
-  return (
-    <div className="App">
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
       <Hello />
       <Welcome name="Buddy" topic="React"/>
       <Message />
-      <Counter />
+      <Counter addTwo = {this.addTwo}/>
     </div>
-  )
+    )
+  }
 }
 
 export default App;
