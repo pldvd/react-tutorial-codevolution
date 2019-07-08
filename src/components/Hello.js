8481d32b0a8e25252ebf0772505991b1cee7efe8
David Pal <paldavid2030@gmail.com>
2019-07-08 10:23:08 +0300

    pracice with React.createElement

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,3 +1,7 @@
 import React from 'react';
 
-export const Hello = () => <h1>Csivvi</h1>;
+
+// so it turns out, that while we use JSX, React invokes the React.createElement method(which takes, at least 3 arguments: the element to be created, it's attribute list wrapped in an object or null if no attributes, and the text content of the element)
+export const Hello = () => {
+  return React.createElement('div',{className: 'myClass'}, React.createElement('h1', null, 'hello This was created by React.createElement'));
+}
