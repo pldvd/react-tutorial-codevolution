427aba58558c88496aa6e5ce8bdd1bcb9b86ef64
David Pal <paldavid2030@gmail.com>
2019-07-12 11:17:51 +0300

    add conditional rendering with ternary operator within render method

--- src/components/UserGreet.js
+++ src/components/UserGreet.js
@@ -10,18 +10,28 @@ class UserGreet extends Component {
   }
 
   render() {
+    //we can simply define a variable name in the render method and set its value conditionally to this.state.isLoggedIn
+
     // const name = this.state.isLoggedIn ? 'Vishwas' : 'Guest'
-    if (this.state.isLoggedIn) {
-      return (
-        <div>
-          Welcome Vishwas
-        </div>
-      )
-    } else {
-      return (
-        <div>Welcome Guest</div>
-      )
-    }
+
+    //we can also conditionally render with an if/else statement within the render method
+    // if (this.state.isLoggedIn) {
+    //   return (
+    //     <div>
+    //       Welcome Vishwas
+    //     </div>
+    //   )
+    // } else {
+    //   return (
+    //     <div>Welcome Guest</div>
+    //   )
+    // }
+    //it is also possible to pass a ternary operator into the jsx
+    return (
+      this.state.isLoggedIn ? 
+      <div>Hello Vishwas</div> : 
+      <div>Hello Guest</div>
+    )
   }
 }
 
